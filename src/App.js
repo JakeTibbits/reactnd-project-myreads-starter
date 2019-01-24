1afbe879634ee1267fcae3e85f44425f8138807f
Jake-Tibbits <jake@bonusmedia.co.uk>
2019-01-24 15:43:06 +0000

    add moveBook function to move books between shelves and update BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
-import BookShelves from './BooksDisplay'
+import Book from './BooksDisplay'
 import BookSearch from './BookSearch'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 class BooksApp extends Component {
   state = {
-    books: []
+    shelves: [
+            {key: 'currentlyReading', title: "Currently Reading", books: []},
+            {key: 'wantToRead', title: "Want to Read", books: []},
+            {key: 'read', title: "Read", books: []},
+            {key: 'none', title:"None", books: []}
+          ]
   }
 
   componentDidMount() {
+    this.shelvesRefresh()
+  }
+
+  shelvesRefresh = () => {
     BooksAPI.getAll().then((books) => {
-      this.setState({ books })
+      let newState = Object.assign({}, this.state)
+
+      for(let shelf of newState.shelves){
+        const shelfBooks = books.filter((book) => {
+          return book.shelf === shelf.key
+        })
+        shelf.books = shelfBooks
+      }
+      this.setState({ newState })
     })
   }
 
 
+  moveBook(book, shelf) {
+    BooksAPI.update(book, shelf).then(()=>{
+      this.shelvesRefresh()
+    })
+  }
+
+
+
   render() {
-    
+    //console.log(this.state.books)
+
     return (
       <div className="app">
 
@@ -28,9 +55,32 @@ class BooksApp extends Component {
 
 
         <Route exact path="/" render={() => (
-          <BookShelves
-            books={this.state.books}
-          />
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                {this.state.shelves.map((shelf) => (
+                  shelf.key !== 'none' && (
+                    <div className="bookshelf" key={shelf.key}>
+                      <h2 className="bookshelf-title">{shelf.title}</h2>
+                      <div className="bookshelf-books">
+                        <ol className="books-grid">
+                          { shelf.books.map((book) => (
+                            <Book key={book.industryIdentifiers[0].identifier} book={book} shelves={this.state.shelves} onMoveBook={(shelf)=>(
+                                this.moveBook(book, shelf)
+                              )}/>
+                          ))}
+                        </ol>
+                      </div>
+                    </div>
+                  )
+                ))}
+              </div>
+              <div className="open-search">
+                <Link to="/search" className="search-button">Add a book</Link>
+              </div>
+            </div>
         )}/>
       </div>
     )
--- src/BooksDisplay.js
+++ src/BooksDisplay.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import Img from 'react-image'
 import VisibilitySensor from 'react-visibility-sensor'
 
 
 class Book extends Component{
+
+  moveBook = (e) => {
+    console.log('change event fired')
+    const newShelf = e.target.value
+    this.props.onMoveBook(newShelf)
+  }
 
   render() {
 
@@ -23,9 +28,6 @@ class Book extends Component{
             }
           }
 
-
-
-
     return(
       <li>
         <VisibilitySensor>
@@ -33,7 +35,7 @@ class Book extends Component{
           <div className="book-top">
             <Img className="book-cover" src={book.imageLinks.thumbnail} loader={imgLoading} unloader={imgNotFound}/>
             <div className="book-shelf-changer">
-              <select defaultValue={book.shelf}>
+              <select defaultValue={book.shelf} onChange={this.moveBook} >
                 <option value="move" disabled>Move to...</option>
                 {shelves.map((shelf) => (
                   <option value={shelf.key} key={shelf.key}>{shelf.title}</option>
@@ -51,55 +53,4 @@ class Book extends Component{
 }
 
 
-
-class BookShelves extends Component {
-
-
-  render() {
-
-    const { books } = this.props,
-          shelves = [
-            {key: 'currentlyReading', title: "Currently Reading"},
-            {key: 'wantToRead', title: "Want to Read"},
-            {key: 'read', title: "Read"},
-            {key: 'none', title:"None"}
-          ],
-          getShelfBooks = (shelf) => {
-            const shelfBooks = books.filter((book) => {
-              return book.shelf === shelf.key
-            })
-            return shelfBooks
-          }
-
-    return(
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          {shelves.map((shelf) => (
-            shelf.key !== 'none' && (
-              <div className="bookshelf" key={shelf.key}>
-                <h2 className="bookshelf-title">{shelf.title}</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    { getShelfBooks(shelf).map((book) => (
-                      <Book key={book.industryIdentifiers[0].identifier} book={book} shelves={shelves} />
-                    ))}
-                  </ol>
-                </div>
-              </div>
-            )
-          ))}
-        </div>
-        <div className="open-search">
-          <Link to="/search" className="search-button">Add a book</Link>
-        </div>
-      </div>
-    )
-  }
-}
-
-
-
-export default BookShelves
+export default Book
